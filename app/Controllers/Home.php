77e5440daeab6022b42e78b5376cc9209cb01da7
FirlyR <firlyrach23@gmail.com>
2023-11-03 11:09:43 +0700

    Add index
    menambahkan

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,6 @@ class Home extends BaseController
 {
     public function index(): string
     {
-        return view('User/Home/index');
+        return view('User/Contact/Section1');
     }
 }
---
+++ app/Views/User/Contact/Section1.php
@@ -0,0 +1,5 @@
+<?= $this->extend('Templates/UserTemplate/Layout') ; ?>
+
+<?= $this->section('content') ; ?>
+<div class="w-auto h-auto text-red-500 text-[2rem]">Percobaan Contact</div>
+<?= $this->endSection() ; ?>
